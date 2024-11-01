f6471fd687e752a53b15672b33eaf84687039f1a
Skyjoe <hairjoe@gmail.com>
2024-11-01 13:56:26 -0300

    Update index.py

--- index.py
+++ index.py
@@ -42,4 +42,5 @@ def get_news():
     return jsonify(news_batch)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
